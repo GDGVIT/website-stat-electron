9e2f8fbd3c7543f1af7cf91cafcab1008eacdb5d
Albert Z <albertzhang66@outlook.com>
2016-10-27 21:32:26 -0500

    Convert bytes into sizes. Closes #2

--- app/size.js
+++ app/size.js
@@ -12,7 +12,7 @@ class Size extends React.Component{
     render(){
             return(
               <div>
-                  <Card content={this.props.size} postdata={this.props.postdata} title={this.props.title}/>
+                  <Card content={convertBytes(this.props.size)} postdata={this.props.postdata} title={this.props.title}/>
               </div>
             );
         }
@@ -21,5 +21,28 @@ class Size extends React.Component{
 Size.propTypes={
   size:React.PropTypes.number
 };
+
+/**
+ * Converts bytes to KB, MB, GB, etc...
+ * @param {number} bytes - The amount of bytes to convert
+ */
+function convertBytes(bytes) {
+    if (bytes === 0 ) {
+        return "0 B";
+    }
+
+    const scale = 1024; // Or 1000?
+    const precision = 1; // Number of decimal places
+    const sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+
+    var labelID = Math.floor(Math.log(bytes) / Math.log(scale));
+
+    if (labelID > sizes.length - 1) {
+        console.error("Size too big!");
+        return "999.9+ YB";
+    }
+
+    return parseFloat(bytes / Math.pow(scale, labelID)).toFixed(precision) + " " + sizes[labelID];
+}
 
 export default Size;
